a1965affd9e7a5af953f4824d6b98991a57e3263
abbydoag <abby.donis.a@gmail.com>
2025-03-15 10:48:44 -0600

    Inciso 8

--- bayes.py
+++ bayes.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 #metrics
 from sklearn.impute import SimpleImputer
-from sklearn.linear_model import BayesianRidge
+from sklearn.linear_model import BayesianRidge, LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.compose import make_column_selector as selector #separa
 from sklearn.compose import ColumnTransformer
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold, cross_val_score, train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import Pipeline
@@ -21,17 +22,17 @@ train_df.dropna(axis=1, how='all', inplace=True)
 #-----------------------------------
 y = train_df.pop('SalePrice')
 X = train_df
+
 y_labels = pd.cut(y, bins=3, labels=['Bajo', 'Medio', 'Alto'])
 X_train, X_test,y_train, y_test = train_test_split(X, y,test_size=0.3,train_size=0.7)
 
 num_cols = X_train.select_dtypes(include=['number']).columns
+cat_cols = X_train.select_dtypes(include=['object']).columns
+
 num_imputer = SimpleImputer(strategy='median')
 X_train[num_cols] = num_imputer.fit_transform(X_train[num_cols])
 X_test[num_cols] = num_imputer.transform(X_test[num_cols])
-
-# Aplicar la mediana solo en esas columnas
-cat_cols = X_train.select_dtypes(include=['object']).columns
-cat_imputer = SimpleImputer(strategy='most_frequent')  # Impute with the most frequent value (mode)
+cat_imputer = SimpleImputer(strategy='most_frequent')
 X_train[cat_cols] = cat_imputer.fit_transform(X_train[cat_cols])
 X_test[cat_cols] = cat_imputer.transform(X_test[cat_cols])
 
@@ -51,13 +52,45 @@ bayes = pipeline.fit(X_train,y_train) #ejecuta modelo que se creeo en el pipelin
 y_pred = bayes.predict(X_test) #da rvariable respuesta ya haceienod las predicciones
 rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 mae = mean_absolute_error(y_test, y_pred)
-print(f"RMSE: {rmse}")
-print(f"MAE: {mae}")
-
-
+print(f"Bayes RMSE: {rmse}")
+print(f"Bayes MAE: {mae}")
+"""
+plt.figure(figsize=(8, 6))
+plt.scatter(y_test, y_pred, color="blue")
+plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], color="red", linewidth=2)
+plt.xlabel("Valores Reales")
+plt.ylabel("Predicciones")
+plt.title("Predicciones vs. Valores Reales (SalePrice)")
+plt.show()
+"""
 
 
 #-----------------------------------
 # 8. validacion cruzada
 #-----------------------------------
+preprocessor_num = Pipeline([
+    ('imputer', SimpleImputer(strategy='median')),
+    ('scaler', StandardScaler())
+])
+preprocessor_cat = Pipeline([
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore', sparse_output=False))
+])
+preprocessor_valC = ColumnTransformer([
+    ('num', preprocessor_num, num_cols),
+    ('cat', preprocessor_cat, cat_cols)
+])
 
+X_transformed = preprocessor_valC.fit_transform(X)
+
+modelo = LinearRegression()
+kfold = KFold(n_splits=5, shuffle=True, random_state=42)
+
+mae_score = cross_val_score(modelo, X_transformed, y, cv=kfold, scoring='neg_mean_absolute_error')
+mae_prom = -mae_score.mean()
+mse_score = cross_val_score(modelo, X_transformed, y, cv=kfold, scoring='neg_mean_squared_error')
+mse_prom = -mse_score.mean()
+rmse = np.sqrt(mse_prom)
+
+print(f"Variacion Cruzada MAE: {mae_prom:.2f}")
+print(f"Variacion Cruzada RMSE: {rmse:.2f}")
